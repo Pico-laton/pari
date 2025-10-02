3c25a05551dadad85a21e2d03e773c0640b31a23
Pico-laton <pavelverdeil@pm.me>
2025-10-02 19:39:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sert tous les fichiers statiques du dossier 'static'
-app.use(express.static('static'));
+app.use(express.static('templates'));
 
 // Si tu veux que la racine serve index.html explicitement :
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/index.html');
+  res.sendFile(__dirname + '/templates/index.html');
 });
 
 app.listen(PORT, () => {
