1ac9a338955889019c8b1a01f8d2bebb88ad5ef7
Pico-laton <pavelverdeil@pm.me>
2025-10-02 19:38:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Route principale
+// Sert tous les fichiers statiques du dossier 'static'
+app.use(express.static('static'));
+
+// Si tu veux que la racine serve index.html explicitement :
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur mon site de paris !');
+  res.sendFile(__dirname + '/static/index.html');
 });
 
 app.listen(PORT, () => {
