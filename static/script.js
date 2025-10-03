6aa19d846afaa3ac83c197aedfd4881c7286cbf3
Pico-laton <pavelverdeil@pm.me>
2025-10-03 18:06:48 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,8 @@
-
+window.onload = function() {
+    const btn = document.createElement('button');
+    btn.textContent = "Voir les cotes";
+    btn.onclick = function() {
+        alert("Les cotes s'affichent bientôt !");
+    };
+    document.body.appendChild(btn);
+};
