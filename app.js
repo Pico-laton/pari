403178a2e810fac7674966fb1cd4832a33d41d45
Pico-laton <pavelverdeil@pm.me>
2025-10-04 12:52:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); goClient;
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
 
+mongoose.connect(uri)
+  .then(() => console.log('✅ Connecté à MongoDB'))
+  .catch(err => console.error('❌ Erreur connexion:', err));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
 // Route pour la racine
@@ -19,6 +28,78 @@ app.get('/:page', (req, res) => {
       res.status(404).send('Page non trouvée');
     }
   });
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAT: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User',userSchema);
+
+// nouveaux utilisateurs 
+app.post('/register',async (req,res)=>{
+  try{
+    const{username,password}=req.body
+    const existingUser = await User.findOne({
+      username: username
+    });
+    if(existingUser){
+      return res.status(400).json({
+        succes:false,
+        message : 'Utilisateur déjà existant'
+      });
+    }
+    const hashPassword = await bcrypt.hash(password,10);
+    const newUser = new User({
+      username,
+      password:hashPassword
+    });
+    await newUser.save();
+    res.json({
+      succes:true,
+      message:'Compte créé avec succès'
+    });
+  } catch (error){
+    console.error('Erreur inscription:',error);
+    res.status(500).json({
+      succes:false,
+      message: 'Erreur serveur'
+    });
+    }
+});
+
+// connexion utilisateur
+app.post('/login',async(req,res)=>{
+  try{
+    const{username,password}=req.body;
+    const user=await User.findOne({username});
+    if (!user){
+      return res.status(400).json({
+        succes:false,
+        message: 'Utilisateur non trouvé'
+      });
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+      return res.status(400).json({
+        succes: false,
+        messgae: 'Mot de passe incorrect'
+      });
+    }
+    res.json({
+      succes:true,
+      message: 'Connexion réussie',
+      user:{username:user.username}
+    });
+  } catch (error) {
+    console.error('Erreur connexion:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Erreur serveur'
+    });
+  }
 });
 
 app.listen(PORT, () => {
